2e3c099025693dc914522d8fd4ff609e9df5d54a
Jesuspinarte <jesuspinarte@gmail.com>
2022-12-28 12:02:36 -0500

    Change dashboard colors

--- init.lua
+++ init.lua
@@ -3,3 +3,10 @@ require("options")
 require("config.lsp")
 
 vim.cmd("colorscheme darkplus")
+
+local hl = vim.api.nvim_set_hl
+
+-- Dashboard
+hl(0, "DashboardHeader", { fg = '#898989', bg = 'NONE' })
+hl(0, "DashboardCenter", { fg = '#EFEFEF', bg = 'NONE' })
+hl(0, "DashboardFooter", { fg = '#FFD11A', bg = 'NONE' })
